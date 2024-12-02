df2544a5ebf750757d0e78f98f22cce14b4303c9
bbrewington <brent.brewington@gmail.com>
2024-12-02 08:41:58 -0500

    2024 day_01 part2

--- 2024/day_01/day_01.py
+++ 2024/day_01/day_01.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 def read_input(file):
     list1, list2 = [], []
     with open(file, 'r') as f:
@@ -11,3 +13,9 @@ def read_input(file):
 def get_list_sum_diff(list1, list2):
     assert len(list1) == len(list2), "lists have different length"
     return sum(abs(list2[i] - list1[i]) for i in range(len(list1)))
+
+def similarity_score(list1, list2):
+    sim_score = 0
+    for l_1 in list1:
+        sim_score += l_1 * Counter(list2)[l_1]
+    return sim_score
--- 2024/day_01/tests/test_read_input.py
+++ 2024/day_01/tests/test_read_input.py
@@ -1,7 +1,9 @@
-from day_01 import read_input, get_list_sum_diff
+from day_01 import read_input, get_list_sum_diff, similarity_score
 
 list1, list2 = read_input('input_example.txt')
 assert get_list_sum_diff(list1, list2) == 11
+assert similarity_score(list1, list2) == 31
 
 list1_final, list2_final = read_input('input_final.txt')
 print(get_list_sum_diff(list1_final, list2_final))
+print(similarity_score(list1_final, list2_final))
