ab5b6592baffb43c0fd7bc6225e31c5c5334ee02
bbrewington <brent.brewington@gmail.com>
2024-12-06 20:03:07 -0500

    2024 day 3 part 2

--- 2024/day_03/day_03.py
+++ 2024/day_03/day_03.py
@@ -11,7 +11,7 @@ def read_input(file_path):
 #     return re.findall("mul\(\d+,\d+\)", x)
 
 def parse_muls(input_str):
-    valid_muls = re.findall("mul\((\d+),(\d+)\)", input_str)
+    valid_muls = re.findall(r"mul\((\d+),(\d+)\)", input_str)
     sum_val = 0
     for mul in valid_muls:
         x, y = [int(x) for x in mul]
@@ -20,3 +20,45 @@ def parse_muls(input_str):
 
 def part1(x):
     return parse_muls(x)
+
+def do_dont_mask(input):
+    do_spans = [x.span() for x in re.finditer(r'do\(\)', input)]
+    dont_spans = [x.span() for x in re.finditer(r"don't\(\)", input)]
+    do_to_dont_spans = [x.span() for x in re.finditer(r"(?<=do\(\)).+?(?=don't\(\))", input)]
+    dont_to_do_spans = [x.span() for x in re.finditer(r"(?=don't\(\)).+?(?<=do\(\))", input)]
+    mask = ['1'] * len(input)
+    for do_span in do_spans:
+        if do_span:
+            start, end = do_span
+            mask[start:end] = ["0"] * (end - start)
+    for dont_span in dont_spans:
+        if dont_span:
+            start, end = dont_span
+            mask[start:end] = ["0"] * (end - start)
+    for do_to_dont_span in do_to_dont_spans:
+        if do_to_dont_span:
+            start, end = do_to_dont_span
+            mask[start:end] = ["1"] * (end - start)
+    for dont_to_do_span in dont_to_do_spans:
+        if dont_to_do_span:
+            start, end = dont_to_do_span
+            mask[start:end] = ["0"] * (end - start)
+    # If last one is a dont (or if that's all there is), mark from there to end of file as 0
+    if (len(do_spans) > 0 and len(dont_spans) > 0):
+        if dont_spans[-1][-1] > do_spans[-1][-1]:
+            mask[dont_spans[-1][-1]:] = ["0"] * len(mask[dont_spans[-1][-1]:])
+    elif (len(do_spans) == 0 and len(dont_spans) > 0):
+        mask[dont_spans[-1][-1]:] = ["0"] * len(mask[dont_spans[-1][-1]:])
+    
+    return ''.join(mask)
+
+def use_do_dont_mask(input):
+    mask = do_dont_mask(input)
+    new_str = ''
+    for i, m in enumerate(mask):
+        if m == '1':
+            new_str += input[i]
+    return new_str
+
+def part2(input):
+    return parse_muls(use_do_dont_mask(input))
--- 2024/day_03/test_day_03.py
+++ 2024/day_03/test_day_03.py
@@ -1,7 +1,26 @@
-from day_03 import read_input, part1
+import pytest
+from day_03 import read_input, part1, do_dont_mask, use_do_dont_mask, part2
 
-input_example = read_input("input_example.txt")
-assert part1(input_example) == 161
+ex_p1 = read_input("input_example_part1.txt")
+assert part1(ex_p1) == 161
 
 input_final = read_input("input_final.txt")
 part1_result = part1(input_final)
+
+ex_p2 = read_input("input_example_part2.txt")
+
+@pytest.mark.parametrize("test_input,expected", [
+    ("adon't()b", "100000000"),
+    ("ado()b", "100001"),
+    ("aado()bbdon't()ccdo()dddon't()ee", "11000011000000000000011000000000")
+])
+def test_do_dont_mask(test_input, expected):
+    assert do_dont_mask(test_input) == expected
+
+def test_use_do_dont_mask():
+    assert use_do_dont_mask("aado()bbdon't()ccdo()dddon't()ee") == "aabbdd"
+
+ex_p3 = read_input("input_final.txt")
+
+def test_part2():
+    assert part2("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))") == 48
